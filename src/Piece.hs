37f0ab30a1b8080fb9259d28df24ee12fb1f8a55
bzuanetti998 <bzuanetti54@gmail.com>
2026-08-06 01:40:47 -0300

    proress on modeling the pieces, trying to figure out the rotation algorithm

--- src/Piece.hs
+++ src/Piece.hs
@@ -11,8 +11,22 @@ data Piece = Piece {
     getKind :: PieceKind,
     getRotation :: Rotation,
     getPosition :: Position,
-    getGrid :: Grid
+    getGrid :: Grid,
+    getHeigth :: Int,
+    getWidth :: Int
 }
+
+getMatrix :: Grid -> [[CellState]]
+getMatrix (Grid matrix) = matrix 
+
+setHeight :: PieceKind -> Int
+setHeight H = 1
+setHeight _ = 2
+
+setWidth :: PieceKind -> Int
+setWidth T = 3
+setWidth S = 2
+setWidth _ = 4
 
 usedSpace :: PieceKind -> Grid
 usedSpace O = Grid[[Empty, Filled],[Empty, Filled],[Empty, Filled],[Filled, Filled]]
@@ -22,25 +36,6 @@ usedSpace R = Grid[[Empty, Filled],[Filled, Filled],[Filled, Empty]]
 usedSpace S = Grid[[Filled, Filled],[Filled, Filled]]
 usedSpace T = Grid[[Empty, Filled],[Filled, Filled], [Empty,Filled]]
 usedSpace H = Grid[[Empty, Filled],[Empty, Filled], [Empty,Filled], [Empty, Filled]]
-
--- vamos tentar generalizar a operação de rotação de uma peça representada por um grid
--- O0deg = Grid[[Empty, Filled],[Empty, Filled],[Empty, Filled],[Filled, Filled]] 
--- O90deg = Grid[[Filled, Filled, Filled, Filled], [Empty, Empty, Empty, Filled]]
--- O180deg = Grid[[Filled, Filled],[Filled, Empty],[Filled, Empty],[Filled, Empty]]
--- O270deg = Grid[[Filled,Empty,Empty,Empty][Filled,Filled,Filled,Filled]]
-
---T0deg = Grid[[Empty,Filled],[Filled,Filled],[Empty,Filled]]
---T90deg = Grid[[Filled,Filled,Filled],[Empty,Filled,Empty]]
---T180deg = Grid[[Filled,Empty],[Filled,Filled],[Filled,Empty]]
---T270deg = Grid[[Empty,Filled,Empty],[Filled,Filled,Filled]]
-
-
--- peça na "horizontal" para peça na "vertical" : 
--- a linha x da matriz rotacionada é formada pela junção dos y-x elementos de cada linha da
--- matriz original aonde y é o tamanho da linha da matriz original e seguindo a ordem de direita pra esquerda
--- peça na "vertical" para peça na "horizontal" :
--- A n° linha da matriz rotacionada é formada pelo n° elemento de cada linha da matriz original
-
 
 changeCellState :: CellState -> CellState --por enquanto é só um not lógico mas pode ser que haja mais lógica depois
 changeCellState Filled = Empty
@@ -62,11 +57,38 @@ rotationDec Deg90  = Deg0
 rotationDec Deg180  = Deg90
 rotationDec Deg270  = Deg180
 
+-- vamos tentar generalizar a operação de rotação de uma peça representada por um grid
+-- O0deg = Grid[[Empty, Filled],[Empty, Filled],[Empty, Filled],[Filled, Filled]] 
+-- O90deg = Grid[[Filled, Filled, Filled, Filled], [Empty, Empty, Empty, Filled]]
+-- O180deg = Grid[[Filled, Filled],[Filled, Empty],[Filled, Empty],[Filled, Empty]]
+-- O270deg = Grid[[Filled,Empty,Empty,Empty][Filled,Filled,Filled,Filled]]
+
+--T0deg = Grid[[Empty,Filled],[Filled,Filled],[Empty,Filled]]
+--T90deg = Grid[[Filled,Filled,Filled],[Empty,Filled,Empty]]
+--T180deg = Grid[[Filled,Empty],[Filled,Filled],[Filled,Empty]]
+--T270deg = Grid[[Empty,Filled,Empty],[Filled,Filled,Filled]]
+
+
+-- peça na "horizontal" para peça na "vertical" : 
+-- a linha x da matriz rotacionada é formada pela junção dos y-x elementos de cada linha da
+-- matriz original aonde y é o tamanho da linha da matriz original e seguindo a ordem de direita pra esquerda
+-- peça na "vertical" para peça na "horizontal" :
+-- A n° linha da matriz rotacionada é formada pelo n° elemento de cada linha da matriz original
+
+rotateAuxR :: Piece  -> Grid
+rotateAuxR origPiece = case (isRotationVertical (getRotation origPiece)) of
+    True -> gridTrue
+    False -> gridFalse
+    where 
+        gridTrue = 
+ 
+    
 rotatePiece :: Piece -> Piece
 rotatePiece origPiece = Piece {
     getKind = getKind origPiece,
     getRotation =  rotationInc (getRotation origPiece),
     getPosition = getPosition origPiece,
-    getGrid = rotateAuxR getGrid origPiece
+    getGrid = rotateAuxR origPiece,
+    getHeigth = getWidth origPiece,  
+    getWidth = getHeight origPiece   
 }
-    where rotateAuxR = 
